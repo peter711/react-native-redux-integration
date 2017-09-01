6cecceee3b3d409fd527d46371784d5ec2cbb93f
peter711 <pfal711@gmail.com>
2017-09-01 22:36:45 +0200

    Added styles

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View, Text, ActivityIndicator} from 'react-native';
+
+import { colors } from '../../styles';
 
 class SplashScreen extends React.Component {
     render() {
         return (
-            <Text>
-                Splash screen
-            </Text>
+            <View style={styles.container}>
+                <ActivityIndicator size={150}/>
+                <Text style={styles.text}>
+                    Loading...
+                </Text>
+            </View>
         );
     }
 
+}
+
+const styles = {
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.blue
+    },
+    text: {
+        paddingTop: 20,
+        fontSize: 18,
+        color: colors.white
+    }
 }
 
 export default SplashScreen;
---
+++ styles/index.js
@@ -0,0 +1,9 @@
+export const colors = {
+    blue: '#011627',
+    red: '#F71735',
+    green: '#41EAD4',
+    white: '#FDFFFC',
+    yellow: '#FF9F1C'
+};
+
+export const styles = {};
